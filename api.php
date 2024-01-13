03282ca81e8a94852e532f6bbc5c9030e566489d
Danila200160 <55765972+Danila200160@users.noreply.github.com>
2024-01-13 20:26:29 +0300

    Create api.php

---
+++ api.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">   
+    <meta name="yandex-verification" content="fb73c8484f7ac7cd" />
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Икра-плов - Главная</title>
+  </head>
+  <body>
+    <div class = "page">
+      <header>
+      <div class = "head">
+        <h1>Икра-плов - Главная</h1>
+<nav>
+  <ul class="topmenu">
+    <li><a href="index.html">Главная</a></li>
+    <li><a href="News.html">Новости</a></li>
+    <li><a href="Services.html">Сервисы</a></li>
+    <li><a href="#" class="down">Приложение</a>
+      <ul class="submenu">
+        <li><a href="Poisk.html">Поиск</a></li>
+        <li><a href="vvod.php">Ввод данных</a></li>
+        <li><a href="stat.php">Статистика</a></li>
+      </ul>
+    </li>
+    <li><a href="Rss.php">Rss</a></li>
+    <li><a href="Sort.php">Буфеты</a></li>
+    <li><a href="XML.php">Выгрузка базы буфетов</a></li>
+    <li><a href="#" class="down">Всякая всячина</a>
+    <ul class="submenu">
+    <li><a href="Kalich.html">Кулинарные изыски</a></li>
+    <li><a href="Kalich2.html">Лучшие повара</a></li>
+    <li><a href="Kalich3.html">Рецептики для дома</a></li>
+     </ul>
+  </ul>
+</nav>
+
+      </div>
+      </header>
+      <div class ="glavnaya">
+        
+      </div>
+      <footer>
+        <div id="podval">
+        </div>
+        <div id= "prava">
+      <h3>Все права защищены &copy; 2023</h3>
+        </div>
+      </footer>
+</div>
+  </body>
+</html>
